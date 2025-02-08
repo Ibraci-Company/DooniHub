9880c9da78cfbec3521f6b0d43a4143bb81ec4e8
Ibrahim CISSE <hello@ibracilinks.com>
2025-02-08 21:51:22 +0000

    Continent: index and show

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContinentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/continents');
 });
+
+Route::resource('/continents', ContinentController::class);
